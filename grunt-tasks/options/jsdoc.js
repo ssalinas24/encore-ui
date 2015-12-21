5ebfa86a7ddd1474bda3eb9eec73ff171180cca5
Droogans <droogans@gmail.com>
2015-12-21 14:49:37 -0500

    fix(jsdoc): Supress publication of private entries

--- grunt-tasks/options/jsdoc.js
+++ grunt-tasks/options/jsdoc.js
@@ -2,7 +2,8 @@ module.exports = {
     rxPageObjects: {
         src: ['src/**/*.page.js', 'src/**/*.exercise.js'],
         options: {
-            destination: 'utils/rx-page-objects/doc/'
+            destination: 'utils/rx-page-objects/doc/',
+            private: false
         }
     }
 };
