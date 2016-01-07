7e51e93edce3fee10584d92b4cfc3d9ae9e89269
Droogans <droogans@gmail.com>
2016-01-07 11:45:31 -0500

    docs(jsdoc): Include long name in nav

--- grunt-tasks/options/jsdoc.js
+++ grunt-tasks/options/jsdoc.js
@@ -3,7 +3,8 @@ module.exports = {
         src: ['src/**/*.page.js', 'src/**/*.exercise.js'],
         options: {
             destination: 'utils/rx-page-objects/doc/',
-            private: false
+            private: false,
+            configure: 'utils/rx-page-objects/jsdoc.conf.json'
         }
     }
 };
