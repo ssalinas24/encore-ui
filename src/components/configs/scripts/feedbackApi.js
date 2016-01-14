f45750694b8a344c43ebad42544d8d1c8435d78b
James Thompson <james.thompson@rackspace.com>
2016-01-14 15:12:19 -0600

    refactor(feedbackApi): FRMW-379-convert to Quark.

--- src/components/configs/scripts/feedbackApi.js
+++
@@ -1,2 +0,0 @@
-angular.module('encore.ui.configs')
-.constant('feedbackApi', '/api/encore/feedback');
---
+++ src/quarks/feedbackApi/scripts/feedbackApi.js
@@ -0,0 +1,8 @@
+angular.module('encore.ui.quarks')
+/**
+ * @ngdoc parameters
+ * @name quarks.constant:feedbackApi
+ * @description
+ * Provides the feedback URL.
+ */
+.constant('feedbackApi', '/api/encore/feedback');
