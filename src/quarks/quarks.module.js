0d2f310368ad81b6aabdcef8f18e18959240602e
James Thompson <james.thompson@rackspace.com>
2016-01-20 13:50:18 -0600

    refactor(quarks): Sprint Docs 10 Quark maintenance.

--- src/quarks/quarks.module.js
+++ src/quarks/quarks.module.js
@@ -7,35 +7,41 @@
  *
  * ## Values & Constants
  * * {@link quarks.value:devicePaths devicePaths}
+ * * {@link quarks.constant:feedbackApi feedbackApi}
+ * * {@link quarks.object:rxStatusColumnIcons rxStatusColumnIcons}
  *
  * ## Filters
- * * {@link quarks.filter:xor xor}
+ * * {@link quarks.filter:rxAge rxAge}
  * * {@link quarks.filter:rxCapitalize rxCapitalize}
  * * {@link quarks.filter:rxDiskSize rxDiskSize}
- * * {@link quarks.filter:rxAge rxAge}
- * * {@link quarks.filter:titleize titleize}
  * * {@link quarks.filter:rxEnvironmentMatch rxEnvironmentMatch}
  * * {@link quarks.filter:rxEnvironmentUrl rxEnvironmentUrl}
- * * {@link quarks.filter:rxUnsafeRemoveHTML rxUnsafeRemoveHTML}
  * * {@link quarks.filter:rxSortEmptyTop rxSortEmptyTop}
+ * * {@link quarks.filter:rxUnsafeRemoveHTML rxUnsafeRemoveHTML}
+ * * {@link quarks.filter:titleize titleize}
+ * * {@link quarks.filter:xor xor}
  *
  * ## Services
- * * {@link quarks.service:hotkeys hotkeys}
- * * {@link quarks.service:SessionStorage SessionStorage}
- * * {@link quarks.service:rxBreadcrumbsSvc rxBreadcrumbsSvc}
- * * {@link quarks.service:SelectFilter SelectFilter}
- * * {@link quarks.service:rxNestedElement rxNestedElement}
  * * {@link quarks.service:Environment Environment}
- * * {@link quarks.service:rxPromiseNotifications rxPromiseNotifications}
- * * {@link quarks.service:rxSortUtil rxSortUtil}
- * * {@link quarks.service:Identity Identity}
- * * {@link quarks.service:rxStatusMappings rxStatusMappings}
  * * {@link quarks.service:ErrorFormatter ErrorFormatter}
- * * {@link quarks.service:rxFeedbackSvc rxFeedbackSvc}
- * * {@link quarks.service:rxLocalStorage rxLocalStorage}
+ * * {@link quarks.service:hotkeys hotkeys}
+ * * {@link quarks.service:Identity Identity}
+ * * {@link quarks.service:NotifyProperties NotifyProperties}
+ * * {@link quarks.service:routesCdnPath routesCdnPath}
  * * {@link quarks.service:rxAutoSave rxAutoSave}
+ * * {@link quarks.service:rxBreadcrumbsSvc rxBreadcrumbsSvc}
+ * * {@link quarks.service:rxBulkSelectUtils rxBulkSelectUtils}
  * * {@link quarks.service:rxDOMHelper rxDOMHelper}
+ * * {@link quarks.service:rxFeedbackSvc rxFeedbackSvc}
+ * * {@link quarks.service:rxFormUtils rxFormUtils}
+ * * {@link quarks.service:rxLocalStorage rxLocalStorage}
+ * * {@link quarks.service:rxNestedElement rxNestedElement}
+ * * {@link quarks.service:rxNotify rxNotify}
+ * * {@link quarks.service:rxPageTitle rxPageTitle}
+ * * {@link quarks.service:rxPromiseNotifications rxPromiseNotifications}
  * * {@link quarks.service:rxScreenshotSvc rxScreenshotSvc}
+ * * {@link quarks.service:rxSortUtil rxSortUtil}
+ * * {@link quarks.service:rxStatusMappings rxStatusMappings}
  */
 angular.module('encore.ui.quarks', [
     'ngResource',
