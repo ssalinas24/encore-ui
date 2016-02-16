642dba5056ca0535f7f00a23df4a3efe9fba807d
Ryan A. Johnson <ryan.johnson@rackspace.com>
2016-02-16 13:43:51 -0600

    chore(rxPermalink): cherry-pick rxPermalink

---
+++ demo/scripts/rxPermalink.js
@@ -0,0 +1,17 @@
+angular.module('demoApp')
+.directive('rxPermalink', function ($location) {
+    return {
+        restrict: 'E',
+        scope: {},
+        transclude: true,
+        template: '<a ng-href="{{permalinkUrl}}" class="permalink" id="{{permaId}}" ng-transclude></a>',
+        link: function (scope, el, attr) {
+            scope.permaId = el.text()
+                .toLowerCase() // normalize to lowercase
+                .replace(/[^0-9a-z]/gi, '-') // non-alphanumeric to hyphen
+                .replace(/\-{2,}/g, '-'); // condense consecutive hyphens
+
+            scope.permalinkUrl = '#' + $location.path() + '#' + scope.permaId;
+        }
+    };
+});
