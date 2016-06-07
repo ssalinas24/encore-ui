22d33488d158383436b1e732e8df443ab0fc89f6
Droogans <andrew.yurisich@rackspace.com>
2016-06-07 17:55:22 -0500

    test(visreg): Update metadata references

--- utils/visual-regression/components/metadata.midway.js
+++ utils/visual-regression/components/metadata.midway.js
@@ -1,11 +1,11 @@
 describe('demo component', function () {
 
     before(function () {
-        demoPage.go('#/components/metadata');
+        demoPage.go('#/components/Metadata');
     });
 
     it('default', function () {
-        screenshot.snap(this, $('.metadata'), { threshold: 0.2 });
+        screenshot.snap(this, $('rx-metadata'), { threshold: 0.2 });
     });
 
 });
