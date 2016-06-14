354f8fd776be19ef66589766b69d1b0f8e4ea173
Freddy Knuth <freddy.knuth@rackspace.com>
2016-06-14 16:21:53 -0500

    feat(domain): Override domain to allow open iframe communication

---
+++ src/domainOverride.js
@@ -0,0 +1,10 @@
+// Currently this is the prefix we will use for all encore applications loaded in an iframe
+var prefix = 'apps.';
+// Let's get the hostname only (no port information)
+var host = window.location.hostname;
+// Find out if we this hostname is prefixed
+var index = host.indexOf(prefix);
+// Get the domain without the prefix if it includes it
+var domain = host.substr((index < 0) ? 0 : index + prefix.length);
+// Override the documnet.domain that allows for explicit iframe communication
+document.domain = domain;
