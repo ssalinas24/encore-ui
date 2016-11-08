4215a6d0adbf2890c08c2223ad89f354dd997561
Ryan A. Johnson <ryan.johnson@rackspace.com>
2016-11-08 13:46:12 -0600

    chore(ngdocs): Add content to index

---
+++ src/index.ngdoc.js
@@ -0,0 +1,18 @@
+/**
+ * @ngdoc overview
+ * @name index
+ * @description
+ * # EncoreUI
+ *
+ * ## Elements
+ * Elements are visible features that contribute to building
+ * larger blocks of functionality in an AngularJS application.
+ *
+ * {@link elements See Elements}
+ *
+ * ## Utilities
+ * Utilities are supporting features that enable functionality
+ * among various Elements.
+ *
+ * {@link utilities See Utilities}
+ */
